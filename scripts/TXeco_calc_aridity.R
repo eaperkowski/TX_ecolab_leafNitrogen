1d83a71913036023ffab2e7e831cef71ee618174
eaperkowski <eaperkowski@gmail.com>
2022-02-19 17:12:52 -0600

    mess with some spei/ai visualizations

--- scripts/TXeco_calc_aridity.R
+++ scripts/TXeco_calc_aridity.R
@@ -28,7 +28,7 @@ pubtheme <- theme_bw() +
 # Import daily weather means, 2006-2020 Normals csv files,
 # and site coordinates
 ################################################################
-daily.means <- read.csv("../data_sheets/TXeco_mesowest_daily.csv",
+monthly.means <- read.csv("../data_sheets/TXeco_climate_monthlymesowest.csv",
                         na.strings = "NA", stringsAsFactors = FALSE)
 normals <- read.csv("../data_sheets/TXeco_climate_normals.csv",
                     na.strings = "NA", stringsAsFactors = FALSE)
@@ -36,61 +36,94 @@ site.coords <- read.csv("../data_sheets/TXeco_sitecoords.csv",
                         na.strings = "NA", stringsAsFactors = FALSE)
 
 ################################################################
-# Subset daily means to 30 days leading up to sampling date and 
-# append site coordinates
+# Join site coords with monthly means
 ################################################################
-# Format sampling.date and date to similar date class
-daily.means$sampling.date <- mdy(daily.means$sampling.date)
-daily.means$date <- ymd(daily.means$date)
+site.coords <- site.coords %>%
+  select(property, latitude, longitude)
 
-# Subset data frame to only include 30 days leading up to sampling 
-# date
-monthly.mean <- daily.means %>%
+# Remove 2021 ecolab sites (until data becomes available)
+site.coords <- site.coords[-c(15:30), ]
+
+
+monthly.means <- monthly.means %>%
   full_join(site.coords) %>%
-  unite("site.long", c(property, sampling.year, visit.type), sep = " ") %>%
-  mutate(month = month(date)) %>%
-  group_by(site.long, month, latitude, longitude) %>%
-  summarize(tmax.mean = mean(max.temp, na.rm = TRUE),
-            tmin.mean = mean(min.temp, na.rm = TRUE),
-            monthly.precip = sum(daily.precip, na.rm = TRUE))
+  unite("site.long", c(property, visit.type), sep = " ")
 
 ################################################################
 # Separate properties into list of data frames. This is necessary
 # because SPEI requires a time series notation that can only
 # be accomplished through a series of unique lists
 ################################################################
-monthly.mean <- setNames(split(x = monthly.mean,
-                              f = monthly.mean$site.long),
-                         paste0(unique(monthly.mean$site.long)))
+monthly.means <- setNames(split(x = monthly.means,
+                              f = monthly.means$site.long),
+                         paste0(unique(monthly.means$site.long)))
 
 # Clean missing data points
-monthly.mean[["Uvalde_2020_02 2021 p"]] <- monthly.mean[["Uvalde_2020_02 2021 p"]][-4,]
-monthly.mean[["Uvalde_2020_02 2021 i"]] <- monthly.mean[["Uvalde_2020_02 2021 i"]][-4,]
+monthly.means[["Uvalde_2020_02 p"]] <- monthly.means[["Uvalde_2020_02 p"]][-8,]
+monthly.means[["Uvalde_2020_02 i"]] <- monthly.means[["Uvalde_2020_02 i"]][-9,]
 
 ################################################################
 # Use Hargreaves equation to estimate 30-day reference 
 # evapotranspiration, merge back to central data frame
 ################################################################
-for(i in seq_along(monthly.mean)) {
-  monthly.mean[[i]]$et0 <- as.numeric(hargreaves(Tmin = monthly.mean[[i]]$tmin.mean,
-                                                Tmax = monthly.mean[[i]]$tmax.mean,
-                                                lat = unique(monthly.mean[[i]]$latitude)))
+for(i in seq_along(monthly.means)) {
+  monthly.means[[i]]$et0 <- as.numeric(hargreaves(Tmin = monthly.means[[i]]$month.tmin,
+                                                  Tmax = monthly.means[[i]]$month.tmax,
+                                                  lat = unique(monthly.means[[i]]$latitude)))
   
-  monthly.mean[[i]]$water.balance <- monthly.mean[[i]]$monthly.precip - monthly.mean[[i]]$et0
-  monthly.mean[[i]]$spei <- spei(data = as.ts(monthly.mean[[i]]$water.balance), scale = 1)$fitted
-  monthly.mean[[i]]$aridity <- monthly.mean[[i]]$monthly.precip / monthly.mean[[i]]$et0
+  monthly.means[[i]]$water.balance <- monthly.means[[i]]$month.precip - monthly.means[[i]]$et0
+  monthly.means[[i]]$spei <- spei(data = as.ts(monthly.means[[i]]$water.balance), scale = 1)$fitted
+  monthly.means[[i]]$aridity <- monthly.means[[i]]$month.precip / monthly.means[[i]]$et0
 }
 
 
 ## Merge list of data.frames into single data.frame
-monthly.mean <- merge_all(monthly.mean)
+monthly.mean <- merge_all(monthly.means)
 
 ## Separate concatenated "site.long" back to three separate columns
 monthly.mean <- separate(monthly.mean, site.long, 
-                        into = c("site", "sampling.year", "visit.type"),
+                        into = c("site", "visit.type"),
                         sep = " ")
 
-monthly.mean$test.spei <- spei(data = as.ts(monthly.mean$water.balance), scale = 1)$fitted
+## Convert month/year to date column
+monthly.mean <- monthly.mean %>%
+  unite("month.year", c(month, year), sep = "/", remove = FALSE) %>%
+  mutate(month.year1 = my(month.year))
+
+
+################################################################
+# Visualize SPEI and AI over time
+################################################################
+ggplot(data = monthly.mean, aes(x = month.year1,
+                                y = spei, color = site,
+                                linetype = visit.type)) +
+  geom_line() +
+  geom_hline(yintercept = 0, linetype = "dotted", size = 1) +
+  facet_wrap(~reorder(site, longitude)) +
+  guides(color = "none") +
+  labs(x = "Date", y = "Standardized Precipitation-Evapotranspiration Index") +
+  theme_bw()
+
+summer.mean <- monthly.mean %>%
+  filter(month == "6"  & year == "2020" & visit.type == "i") %>%
+  group_by(site, visit.type, longitude) %>%
+  summarize(mean.spei = mean(spei, na.rm = TRUE),
+            sd.spei = sd(spei, na.rm = TRUE),
+            mean.ai = mean(aridity, na.rm = TRUE),
+            sd.ai = sd(aridity, na.rm = TRUE))
+
+ggplot(data = summer.mean, 
+       aes(x = reorder(site, longitude),
+           y = mean.spei, fill = site)) +
+  geom_bar(stat = "identity") +
+  facet_grid(~visit.type) +
+  guides(color = "none") +
+  labs(x = "Site", y = "SPEI") +
+  scale_y_continuous(limits = c(-2,2), breaks = seq(-2, 2, 0.5)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
 
 ################################################################
 # Visualize 30-day aridity index
@@ -107,7 +140,7 @@ init.2021 <- ggplot(data = subset(monthly.mean, visit.type == "i" &
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12))
 
 
-init.2020 <-ggplot(data = subset(monthly.mean, visit.type == "i" & 
+init.2020 <- ggplot(data = subset(monthly.mean, visit.type == "i" & 
                                    sampling.year == "2020"), 
                    aes(x = reorder(site, longitude), y = spei)) +
   geom_bar(stat = "identity", position = "dodge") +
