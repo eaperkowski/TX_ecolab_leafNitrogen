0fc6c81cc193ada3c9772e9e9102be90074283a4
eaperkowski <evan.a.perkowski@ttu.edu>
2022-03-10 16:19:20 -0600

    revise data file creation file and minor syntax changes to dry biomass file

--- scripts/TXeco_create_datafile.R
+++ scripts/TXeco_create_datafile.R
@@ -4,21 +4,82 @@
 # Load libraries
 library(dplyr)
 library(tidyr)
+library(LeafArea)
+library(readxl)
+library(reshape)
 
-# Import datasheets
-spei <- read.csv("../data_sheets/TXeco_spei_data.csv",
-                 stringsAsFactors = FALSE,
-                 na.strings = "NA")
-spei <- dplyr::select(spei, site:visit.type, norm.precip, norm.spei, norm.aridity,
-               month.precip, spei, aridity)
-leaf <- read.csv("../data_sheets/TXeco_leaftraits.csv")
+##########################################################################
+## Import raw leaf trait data files
+##########################################################################
+biomass <- read.csv("../data_sheets/TXeco_drybiomass.csv", na.strings = "NA")
+
+##########################################################################
+## Import soil data, spei data, site coord data, and species id data
+##########################################################################
+spei <- read.csv("../data_sheets/TXeco_spei_data.csv", 
+                 stringsAsFactors = FALSE, na.strings = "NA")
+
+spei <- dplyr::select(spei, site:visit.type, norm.precip, norm.spei, 
+                      norm.aridity, month.precip, spei, aridity)
+
 site.coords <- read.csv("../data_sheets/TXeco_sitecoords.csv")
+
 spp.info <- read.csv("../data_sheets/TXeco_species_id.csv", 
                      na.strings = c("NA", "<NA>", ""))
+
 soil <- read.csv("../data_sheets/TXeco_soil_characteristics.csv")
 
-# Load calc_chi
+##########################################################################
+## Load calc_chi function
+##########################################################################
 source("/Users/eaperkowski/git/r_functions/calc_chi.R")
+
+##########################################################################
+## Import raw costech files
+##########################################################################
+file.list <- list.files(path = "/Users/eaperkowski/OneDrive - Texas Tech University/TXeco_leafnitrogen/cn_results/",
+                        recursive = TRUE,
+                        pattern = "\\.xlsx$",
+                        full.names = TRUE)
+
+file.list <- setNames(file.list, file.list)
+cn.plates <- lapply(file.list, read_xlsx)
+
+##########################################################################
+## Merge raw costech files, remove QC and standards, and include only
+## %N and %C
+##########################################################################
+cn.plates <- cn.plates %>%
+  merge_all() %>%
+  filter(sample.type == "unknown" & sample.id != "QC") %>%
+  select(id = sample.id, n.leaf = nitrogen.weight.percent, 
+         c.leaf = carbon.weight.percent)
+
+##########################################################################
+## Quantify leaf area using leaf area images
+##########################################################################
+ij.path <- "/Applications/ImageJ.app"
+imagepath <- "/Users/eaperkowski/git/TX_ecolab_leafNitrogen/leaf_area/"
+
+leaf.area <- run.ij(path.imagej = ij.path,
+                    set.directory = imagepath,
+                    distance.pixel = 117.9034,
+                    known.distance = 1,
+                    set.memory = 300, low.size = 0.1)
+names(leaf.area)[1] <- "id"
+
+##########################################################################
+## Append leaf areas to biomass and cn plate data, calculate sla and narea
+##########################################################################
+leaf <- biomass %>%
+  full_join(leaf.area) %>%
+  full_join(cn.plates) %>%
+  mutate(sla = (total.leaf.area / dry.wgt),
+         narea = (n.leaf / sla) * 10000)
+
+## Stopped here 03/10/2022 ##
+
+
 
 # Separate site in leaf data.frame, merge with site.coords to get property name
 test <- leaf %>%
