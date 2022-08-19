963bc06d1610a28504f3cfd63f8e152385053d2b
eaperkowski <eaperkowski@gmail.com>
2022-08-19 13:28:56 -0500

    add updated figs and talk from ESA

--- scripts/TXeco_dataanalysis_crude.R
+++ scripts/TXeco_dataanalysis_crude.R
@@ -117,11 +117,11 @@ beta.h2o.pred <- data.frame(get_model_data(beta.60, type = "pred",
 beta.h2o.inter <- data.frame(get_model_data(beta.60, type = "pred", 
                                             terms = c("wn.60", "pft")))
 
+set.seed(5)
 beta.no3n.ind <- ggplot(data = subset(df, !is.na(pft)), 
                     aes(x = soil.no3n, y = log(beta))) +
   geom_jitter(aes(fill = pft),
               width = 0.5, size = 3, alpha = 0.7, shape = 21) +
-  geom_hline(yintercept = 5, lty = 2, lwd = 1) +
   geom_ribbon(data = beta.no3n.pred, 
               aes(x = x, y = predicted, ymin = log(conf.low), 
                   ymax = log(conf.high)), alpha = 0.25) +
@@ -138,7 +138,8 @@ beta.no3n.ind <- ggplot(data = subset(df, !is.na(pft)),
        fill = "Functional group") +
   theme_bw(base_size = 18) +
   theme(legend.text.align = 0,
-        panel.border = element_rect(size = 1.25))
+        panel.border = element_rect(size = 1.25),
+        panel.grid = element_blank())
 beta.no3n.ind
 
 png("../working_drafts/TXeco_beta_no3n_ind.png",
@@ -166,12 +167,12 @@ beta.h2o.ind <- ggplot(data = subset(df, !is.na(pft)),
        fill = "Functional group") +
   theme_bw(base_size = 18) +
   theme(legend.text.align = 0,
-        panel.border = element_rect(size = 1.25))
+        panel.border = element_rect(size = 1.25),
+        panel.grid = element_blank())
 beta.h2o.ind
 
 beta.h2o.int <- ggplot(data = subset(df, !is.na(pft)), 
                           aes(x = wn.60, y = log(beta))) +
-  geom_hline(yintercept = 5, lty = 2, lwd = 1) +
   geom_jitter(aes(fill = pft),
               width = 0.1, size = 3, alpha = 0.7, shape = 21) +
   geom_ribbon(data = subset(beta.h2o.inter, group == "c3_legume"), 
@@ -200,7 +201,8 @@ beta.h2o.int <- ggplot(data = subset(df, !is.na(pft)),
        fill = "Functional group") +
   theme_bw(base_size = 18) +
   theme(legend.text.align = 0,
-        panel.border = element_rect(size = 1.25))
+        panel.border = element_rect(size = 1.25),
+        panel.grid = element_blank())
 beta.h2o.int
 
 png("../working_drafts/TXeco_beta_h2o_ind.png",
@@ -231,9 +233,9 @@ df <- read.csv("../data_sheets/TXeco_compiled_datasheet.csv",
 df$pft <- factor(df$pft, levels = c("c3_legume", "c4_nonlegume", "c3_nonlegume"))
 
 
-df$chi[c(11, 62, 315)] <- NA
+df$chi[c(11, 62, 315, 402)] <- NA
 
-chi <- lmer(chi ~ wn.60 * soil.no3n * pft + log(beta) + 
+chi <- lmer(chi ~ wn.60 * soil.no3n * pft  + 
               (1 | NCRS.code), data = df)
 
 # Check model assumptions
@@ -252,8 +254,15 @@ r.squaredGLMM(chi)
 ## Single factor effect of wn.60 on chi
 test(emtrends(chi, ~1, "wn.60"))
 
+## Effect of wn.60 on chi across species
+test(emtrends(chi, ~pft, "wn.60"))
+emmeans(chi, ~pft, "wn.60", at = list(wn.60=0))
+
 ## Single factor effect of soil.no3n on chi
 test(emtrends(chi, ~1, "soil.no3n"))
+
+## Single factor effect of beta on chi
+test(emtrends(chi, ~1, "beta"))
 
 ## Single factor effect of soil.no3n on chi
 cld(emmeans(chi, pairwise~pft))
@@ -273,6 +282,7 @@ chi.h2o.pred <- data.frame(get_model_data(chi, type = "pred",
 chi.h2o.inter <- data.frame(get_model_data(chi, type = "pred", 
                                            terms = c("wn.60", "pft")))
 
+set.seed(5)
 chi.no3n.ind <- ggplot(data = subset(df, !is.na(pft)), 
                       aes(x = soil.no3n, y = chi)) +
   geom_jitter(aes(fill = pft),width = 0.7, size = 3, 
@@ -293,12 +303,15 @@ chi.no3n.ind <- ggplot(data = subset(df, !is.na(pft)),
        fill = "Functional group") +
   theme_bw(base_size = 18) +
   theme(legend.text.align = 0,
-        panel.border = element_rect(size = 1.25))
+        panel.border = element_rect(size = 1.25),
+        panel.grid = element_blank())
+chi.no3n.ind
 
 chi.no3n.int <- ggplot(data = subset(df, !is.na(pft)), 
                       aes(x = soil.no3n, y = chi)) +
   geom_jitter(aes(fill = pft),
-              width = 0.1, size = 3, alpha = 0.7, shape = 21) +
+              width = 0.7, size = 3, alpha = 0.7, shape = 21) +
+  
   geom_ribbon(data = subset(chi.no3n.inter, group == "c3_legume"), 
               aes(x = x, y = predicted, ymin = conf.low, 
                   ymax = conf.high), alpha = 0.25, fill = cbbPalette3[1]) +
@@ -325,9 +338,9 @@ chi.no3n.int <- ggplot(data = subset(df, !is.na(pft)),
        fill = "Functional group") +
   theme_bw(base_size = 18) +
   theme(legend.text.align = 0,
-        panel.border = element_rect(size = 1.25))
+        panel.border = element_rect(size = 1.25),
+        panel.grid = element_blank())
 chi.no3n.int  
-
 
 chi.h2o.ind <- ggplot(data = subset(df, !is.na(pft)), 
                       aes(x = wn.60, y = chi)) +
@@ -349,12 +362,14 @@ chi.h2o.ind <- ggplot(data = subset(df, !is.na(pft)),
        fill = "Functional group") +
   theme_bw(base_size = 18) +
   theme(legend.text.align = 0,
-        panel.border = element_rect(size = 1.25))
-  
+        panel.border = element_rect(size = 1.25),
+        panel.grid = element_blank())
+chi.h2o.ind
+
 chi.h2o.int <- ggplot(data = subset(df, !is.na(pft)), 
                        aes(x = wn.60, y = chi)) +
   geom_jitter(aes(fill = pft),
-              width = 0.1, size = 3, alpha = 0.7, shape = 21) +
+              width = 0.7, size = 3, alpha = 0.7, shape = 21) +
   geom_ribbon(data = subset(chi.h2o.inter, group == "c3_legume"), 
               aes(x = x, y = predicted, ymin = conf.low, 
                   ymax = conf.high), alpha = 0.25, fill = cbbPalette3[1]) +
@@ -364,7 +379,7 @@ chi.h2o.int <- ggplot(data = subset(df, !is.na(pft)),
               aes(x = x, y = predicted, ymin = conf.low, 
                   ymax = conf.high), alpha = 0.25, fill = cbbPalette3[2]) +
   geom_line(data = subset(chi.h2o.inter, group == "c4_nonlegume"), size = 1,
-            aes(x = x, y = predicted), color = cbbPalette3[2], lty = 2) +
+            aes(x = x, y = predicted), color = cbbPalette3[2]) +
   geom_ribbon(data = subset(chi.h2o.inter, group == "c3_nonlegume"), 
               aes(x = x, y = predicted, ymin = conf.low, 
                   ymax = conf.high), alpha = 0.25, fill = cbbPalette3[3]) +
@@ -377,14 +392,13 @@ chi.h2o.int <- ggplot(data = subset(df, !is.na(pft)),
   scale_x_continuous(limits = c(20, 120), breaks = seq(20, 120, 20)) +
   scale_y_continuous(limits = c(0.2, 1), breaks = seq(0.2, 1, 0.2)) +
   labs(x = expression(bold("60-day mean daily soil moisture (mm)")),
-       y = expression(bold(ln~beta)),
+       y = expression(bold(chi)),
        fill = "Functional group") +
   theme_bw(base_size = 18) +
   theme(legend.text.align = 0,
-        panel.border = element_rect(size = 1.25))
+        panel.border = element_rect(size = 1.25),
+        panel.grid = element_blank())
 chi.h2o.int
-
-
 
 png("../working_drafts/TXeco_chi_h2o_int.png",
     width = 8, height = 5, units = 'in', res = 600)
@@ -423,10 +437,9 @@ df <- read.csv("../data_sheets/TXeco_compiled_datasheet.csv",
          chi = ifelse(chi > 0.95 | chi < 0.20, NA, chi))
 df$pft <- factor(df$pft, levels = c("c3_legume", "c4_nonlegume", "c3_nonlegume"))
 
+df$narea[df$narea > 5] <- NA
 
-df$narea[c(80, 156, 273, 509)] <- NA
-
-narea <- lmer(log(narea) ~ wn.60 * soil.no3n * pft + chi + beta + (1 | NCRS.code),
+narea <- lmer(narea ~ wn.60 * soil.no3n * pft + chi + beta + (1 | NCRS.code),
               data = df)
 
 # Check model assumptions
@@ -445,45 +458,50 @@ r.squaredGLMM(narea)
 
 ## Individual beta effect
 test(emtrends(narea, ~1, "beta"))
+emmeans(narea, ~1, at = list(beta = 0))
+
 
 ## Individual chi effect
-test(emtrends(narea, ~1, "chi"))
-
+test(emtrends(narea, ~pft, "soil.no3n"))
 
 ##########################################################################
 ## Narea plots
 ##########################################################################
 narea.beta.pred <- as.data.frame(get_model_data(narea, type = "pred", 
                                                 terms = "beta"))
-
-
-narea.chi.pred <- data.frame(get_model_data(narea, type = "pred", terms = "chi"))
+narea.chi.pred <- data.frame(get_model_data(narea, type = "pred", 
+                                            terms = "chi"))
+narea.soiln.pred <- data.frame(get_model_data(narea, type = "pred", 
+                                              terms = "soil.no3n"))
 
 narea.beta.plot <- ggplot(data = subset(df, !is.na(pft)), 
-                         aes(x = beta, y = log(narea))) +
-  geom_jitter(aes(fill = pft),width = 0.5, size = 3, alpha = 0.7, shape = 21) +
+                         aes(x = log(beta), y = log(narea))) +
+  geom_jitter(aes(fill = pft), width = 0.5, size = 3, alpha = 0.7, shape = 21) +
   geom_ribbon(data = narea.beta.pred,
-              aes(x = x, y = log(predicted), ymin = log(conf.low), 
+              aes(x = log(x), y = log(predicted), 
+                  ymin = log(conf.low), 
                   ymax = log(conf.high)), alpha = 0.25) +
   geom_line(data = narea.beta.pred, size = 1,
-            aes(x = x, y = log(predicted))) +
+            aes(x = log(x), y = log(predicted))) +
   scale_fill_manual(values = c(cbbPalette3), 
                     labels = c(expression("C"[3]~"legume"),
                                expression("C"[4]~"non-legume"),
                                expression("C"[3]~"non-legume"))) +
-  scale_x_continuous(limits = c(0, 750), breaks = seq(0, 750, 250)) +
-  scale_y_continuous(limits = c(-1, 3), breaks = seq(-1, 3, 1)) +
-  labs(x = expression(bold(beta)),
-       y = expression(ln~" N"[area]),
+  scale_x_continuous(limits = c(-2.5, 7.5), breaks = seq(-2.5, 7.5, 2.5)) +
+  scale_y_continuous(limits = c(-1, 2), breaks = seq(-1, 2, 1)) +
+  labs(x = expression(bold(ln~beta)),
+       y = expression(bold(ln)~"N"[area]),
        fill = "Funtional group") +
   theme_bw(base_size = 18) +
-  theme(legend.text.align = 0)
+  theme(legend.text.align = 0,
+        panel.grid = element_blank())
+narea.beta.plot
 
 narea.chi.plot <- ggplot(data = subset(df, !is.na(pft)), 
                          aes(x = chi, y = log(narea))) +
   geom_jitter(aes(fill = pft), width = 0.01, size = 3, alpha = 0.7, shape = 21) +
   geom_ribbon(data = narea.chi.pred,
-              aes(x = x, y = predicted, ymin = log(conf.low), 
+              aes(x = x, y = log(predicted), ymin = log(conf.low), 
                   ymax = log(conf.high)), alpha = 0.25) +
   geom_line(data = narea.chi.pred, size = 1,
             aes(x = x, y = log(predicted)), lty = 2) +
@@ -492,12 +510,36 @@ narea.chi.plot <- ggplot(data = subset(df, !is.na(pft)),
                                expression("C"[4]~"non-legume"),
                                expression("C"[3]~"non-legume"))) +
   scale_x_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.2)) +
-  scale_y_continuous(limits = c(-1, 3), breaks = seq(-1, 3, 1)) +
+  scale_y_continuous(limits = c(-1, 2), breaks = seq(-1, 2, 1)) +
   labs(x = expression(bold(chi)),
-       y = expression(bold(ln~"N"[area])),
+       y = expression(bold(ln)~"N"[area]),
        fill = "Functional type") +
   theme_bw(base_size = 18) +
-  theme(legend.text.align = 0)
+  theme(legend.text.align = 0,
+        panel.grid = element_blank())
+narea.chi.plot
+
+narea.soilno3n.plot <- ggplot(data = subset(df, !is.na(pft)), 
+                                  aes(x = soil.no3n, y = log(narea))) +
+  geom_jitter(aes(fill = pft), width = 0.01, size = 3, alpha = 0.7, shape = 21) +
+  geom_ribbon(data = narea.soiln.pred,
+              aes(x = x, y = log(predicted), ymin = log(conf.low),
+                  ymax = log(conf.high)), alpha = 0.25) +
+  geom_line(data = narea.soiln.pred, 
+            size = 1, aes(x = x, y = log(predicted)), lty = 2) +
+  scale_fill_manual(values = cbbPalette3, 
+                    labels = c(expression("C"[3]~"legume"),
+                               expression("C"[4]~"non-legume"),
+                               expression("C"[3]~"non-legume"))) +
+  scale_y_continuous(limits = c(-1, 2), breaks = seq(-1, 2, 1)) +
+  labs(x = expression(bold("Soil nitrogen availability (ppm NO"[3]~"-N)")),
+       y = expression(bold(ln)~"N"[area]),
+       fill = "Functional type") +
+  theme_bw(base_size = 18) +
+  theme(legend.text.align = 0,
+        panel.grid = element_blank())
+
+
 
 
 png("../working_drafts/TXeco_narea_beta.png",
@@ -510,15 +552,24 @@ png("../working_drafts/TXeco_narea_chi.png",
 narea.chi.plot
 dev.off()
 
+png("../working_drafts/TXeco_narea_soiln.png",
+    width = 8, height = 5, units = 'in', res = 600)
+narea.soilno3n.plot
+dev.off()
+
+png("../working_drafts/TXeco_narea_soiln_int.png",
+    width = 8, height = 5, units = 'in', res = 600)
+narea.soilno3n.int.plot
+dev.off()
+
 ##########################################################################
 ## Narea
 ##########################################################################
-df$n.leaf[c(344, 462)] <- NA
+df$n.leaf[c(509)] <- NA
 
 
-nmass <- lmer(log(n.leaf) ~ ai.60 * log(beta) * soil.no3n * pft + 
-                (1 | sampling.year) + (1 | NCRS.code),
-              data = df)
+nmass <- lmer(log(n.leaf) ~ wn.60 * soil.no3n * pft + beta + chi + 
+                (1 | NCRS.code), data = df)
 
 # Check model assumptions
 plot(nmass)
@@ -532,4 +583,9 @@ outlierTest(nmass)
 summary(nmass)
 Anova(nmass)
 r.squaredGLMM(nmass)
+
+test(emtrends(nmass, ~1, "wn.60"))
+test(emtrends(nmass, ~1, "soi.no3n"))
+
+test(emtrends(nmass, ~wn.60, "soil.no3n", at = list(wn.60 = c(0, 40, 80, 120))))
 
